9512f9fe00eee00a9cc05b998a92c84be67be740
benjika <bennykatz1012@gmail.com>
2018-04-28 09:16:36 +0300

    added drawer

--- app/src/main/java/com/example/luput/tnt/MainActivity.java
+++ app/src/main/java/com/example/luput/tnt/MainActivity.java
@@ -1,15 +1,33 @@
 package com.example.luput.tnt;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()== android.R.id.home){
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
