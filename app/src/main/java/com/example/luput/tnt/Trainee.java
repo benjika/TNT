b0d2bce7ea73eeaf673bd1052705550be8dfe100
bennykatz1012 <bennykatz1012@gmail.com>
2018-07-09 22:10:02 +0300

    09/07

--- app/src/main/java/com/example/luput/tnt/Trainee.java
+++ app/src/main/java/com/example/luput/tnt/Trainee.java
@@ -6,7 +6,8 @@ public class Trainee extends Person {
 
     int TRAINEE_ID = 2;
 
-    public Trainee(String firstName, String lastName, Bitmap photo, String gender, String phone, String dayOfBirth, String homeAddress, String emailAddress) {
+    public Trainee(String firstName, String lastName, Bitmap photo, String gender, String phone,
+                   String dayOfBirth, String homeAddress, String emailAddress) {
         super(firstName, lastName, photo, gender, phone, dayOfBirth, homeAddress, emailAddress);
     }
 
