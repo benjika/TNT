768bfbd606a80646c212961e34cb935bc2be75e3
bennykatz1012 <bennykatz1012@gmail.com>
2018-07-14 22:15:06 +0300

    new add trainee by id dialog

--- app/src/main/java/com/example/luput/tnt/CoachFragment.java
+++ app/src/main/java/com/example/luput/tnt/CoachFragment.java
@@ -2,18 +2,16 @@ package com.example.luput.tnt;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Color;
-import android.net.Uri;
-import android.os.Build;
-import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,10 +29,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-import static java.util.Collections.*;
 import static java.util.Collections.sort;
 
 public class CoachFragment extends Fragment implements View.OnClickListener {
@@ -129,10 +125,10 @@ public class CoachFragment extends Fragment implements View.OnClickListener {
 
     public void addTraineeDialog() {
         addTraineeDialog = new Dialog(getView().getContext());
-        addTraineeDialog.setContentView(R.layout.add_trainee_dialog);
+        addTraineeDialog.setContentView(R.layout.coach_add_trainee_email_dialog);
         addTraineeDialog.setTitle("Add trainee");
 
-        final Button addTrainee = (Button) addTraineeDialog.findViewById(R.id.coach_addTrainee_dialog_BTNAdd);
+        /*final Button addTrainee = (Button) addTraineeDialog.findViewById(R.id.coach_addTrainee_dialog_BTNAdd);
         Button cancelTrainee = (Button) addTraineeDialog.findViewById(R.id.coach_addTrainee_dialog_BTNCancel);
         final EditText addtraineeET = (EditText) addTraineeDialog.findViewById(R.id.coach_addTrainee_ET);
         addTrainee.setEnabled(true);
@@ -181,7 +177,7 @@ public class CoachFragment extends Fragment implements View.OnClickListener {
                 addTraineeDialog.cancel();
             }
         });
-        addTraineeDialog.show();
+        addTraineeDialog.show();*/
     }
 
     private ArrayList<Trainee> traineesOfThisCoach(final ViewGroup container, final View view) {
@@ -250,7 +246,8 @@ public class CoachFragment extends Fragment implements View.OnClickListener {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.coach_menu_item_email:
-                Toast.makeText(context, "added by email", Toast.LENGTH_SHORT).show();
+              inflateEmailDialog();
+               // Toast.makeText(context, "added by email", Toast.LENGTH_SHORT).show();
                 floatingActionMenu.close(true);
                 break;
             case R.id.coach_menu_item_mobile:
@@ -262,4 +259,61 @@ public class CoachFragment extends Fragment implements View.OnClickListener {
 
     }
 
+    public void inflateEmailDialog() {
+        LayoutInflater layoutInflaterAndroid = LayoutInflater.from(context);
+        View mView = layoutInflaterAndroid.inflate(R.layout.coach_add_trainee_email_dialog, null);
+        AlertDialog.Builder alertDialogBuilderUserInput = new AlertDialog.Builder(context);
+        alertDialogBuilderUserInput.setView(mView);
+
+        final TextInputEditText addtraineeET = (TextInputEditText) mView.findViewById(R.id.coach_add_trainee_email);
+        alertDialogBuilderUserInput
+                .setCancelable(false)
+                .setPositiveButton("Send", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogBox, int id) {
+                        final String emailToAdd = addtraineeET.getText().toString();
+                        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                for (DataSnapshot trainee : dataSnapshot.child("trainee").getChildren()) {
+                                    Trainee currTraine = trainee.getValue(Trainee.class);
+                                    if (currTraine.getEmailAddress().equals(emailToAdd)) {
+                                        key = trainee.getKey();
+                                        break;
+                                    }
+                                }
+                                if (key != null) {
+                                    Coach coach = dataSnapshot.child("coach").child(UserID).getValue(Coach.class);
+                                    if (coach.getTrainees() != null) {
+                                        ArrayList<String> newlist = new ArrayList<String>(coach.getTrainees());
+                                        newlist.add(key);
+                                        coach.setTrainees(newlist);
+                                    } else {
+                                        ArrayList<String> newlist = new ArrayList<String>();
+                                        newlist.add(key);
+                                        coach.setTrainees(newlist);
+                                    }
+                                    mDatabase.child("coach").child(UserID).setValue(coach);
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+
+                            }
+                        });
+                    }
+                });
+        alertDialogBuilderUserInput.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener()
+
+                {
+                    public void onClick(DialogInterface dialogBox, int id) {
+                        dialogBox.cancel();
+                    }
+                });
+
+        AlertDialog alertDialogAndroid = alertDialogBuilderUserInput.create();
+        alertDialogAndroid.show();
+    }
 }
+
